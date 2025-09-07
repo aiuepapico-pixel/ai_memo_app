a3db50e3ac84fc8644d383fa2c137b91153ae907
aiuepapico-pixel <aiue.papi.co@gmail.com>
2025-09-07 11:26:09 +0900

    テーブル作成

---
+++ app/Models/Memo.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * メモモデル
+ */
+class Memo extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+    ];
+
+    /**
+     * メモの所有者を取得
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
